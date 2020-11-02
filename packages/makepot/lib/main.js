69af4b35ef46ea6c031b518c69b06b8a83312136
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2020-11-02 17:12:35 +0000

    Build JS

---
+++ packages/makepot/lib/main.js
@@ -0,0 +1,93 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const exec_1 = require("@actions/exec");
+const io = __importStar(require("@eventespresso-actions/io"));
+const utils_1 = require("@eventespresso-actions/utils");
+const utils_2 = require("./utils");
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { exclude, headers, headersJsonFile, ignoreDomain, include, packageName, savePath, slug, textDomain, } = utils_2.getInput();
+        try {
+            //#region WP CLI setup
+            core.startGroup('Setup WP-CLI');
+            const wpcliPath = 'wp-cli.phar';
+            // download WP CLI executable
+            const error = yield utils_1.downloadUrl('https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar', wpcliPath);
+            if (error) {
+                throw new Error(error);
+            }
+            /**
+             * make the file executable
+             * @see https://nodejs.org/api/fs.html#fs_fs_chmod_path_mode_callback
+             */
+            yield io.chmod(wpcliPath, 0o765);
+            // move to path
+            yield exec_1.exec('sudo mv', [wpcliPath, '/usr/local/bin/wp']);
+            core.endGroup();
+            //#endregion
+            //#region Configuration
+            const potPath = `${savePath}/${textDomain}.pot`;
+            let potHeaders;
+            if (headersJsonFile && io.existsSync(headersJsonFile)) {
+                // JSON file may have spaces and new lines
+                // we will parse it and stringify again to get rid of spaces and line breaks
+                const headersObj = JSON.parse(io.readFileSync(headersJsonFile, { encoding: 'utf8' }));
+                potHeaders = JSON.stringify(headersObj);
+            }
+            else {
+                potHeaders = headers;
+            }
+            // command arguments
+            const args = [
+                `--slug=${slug}`,
+                exclude && `--exclude=${exclude}`,
+                potHeaders && `--headers=${potHeaders}`,
+                ignoreDomain && '--ignore-domain',
+                include && `--include=${include}`,
+                packageName && `--package-name="${packageName}"`,
+                textDomain && `--domain=${textDomain}`,
+            ].filter(Boolean);
+            //#endregion
+            //#region POT file generation
+            core.startGroup('Generating POT File');
+            yield exec_1.exec('wp i18n make-pot .', [potPath, ...args, `--allow-root`]);
+            // temporary - output POT file contents
+            core.info(io.readFileSync(potPath, { encoding: 'utf8' }));
+            core.endGroup();
+            //#endregion
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+run();
---
+++ packages/utils/lib/download.js
@@ -0,0 +1,45 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.downloadUrl = void 0;
+const https_1 = __importDefault(require("https"));
+const fs_1 = __importDefault(require("fs"));
+/**
+ * Download the given URL to the given destination.
+ */
+exports.downloadUrl = (url, dest) => {
+    return new Promise((resolve, reject) => {
+        const file = fs_1.default.createWriteStream(dest, { flags: 'wx' });
+        const request = https_1.default.get(url, (response) => {
+            // make sure we have a valid status code
+            if (response.statusCode === 200) {
+                response.pipe(file);
+            }
+            else {
+                file.close();
+                fs_1.default.unlink(dest, () => null);
+                reject(`Download failed! Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+        });
+        request.on('error', (err) => {
+            file.close();
+            fs_1.default.unlink(dest, () => null);
+            reject(err.message);
+        });
+        file.on('finish', () => {
+            resolve();
+        });
+        file.on('error', (err) => {
+            file.close();
+            if (err.name === 'EEXIST') {
+                reject('File already exists');
+            }
+            else {
+                fs_1.default.unlink(dest, () => null);
+                reject(err.message);
+            }
+        });
+    });
+};
