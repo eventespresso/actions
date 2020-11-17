c4d8e17ae4c0106f90e0567f18b4b53a6d014faa
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-11-17 22:34:02 +0530

    Version bump refactor action (#8)
    * Simplify version bump to use SRP
    
    * Update tests
    
    * Update action.yml

--- packages/version-bump/src/tests/utils.ts
+++ packages/version-bump/src/tests/utils.ts
@@ -31,7 +31,8 @@ export const getMockedFileContents = (path: string, version?: string): string =>
 type MainTestCase = {
 	type: BumpType;
 	inputVer: string;
-	outputVer?: string;
+	outputVer: string;
+	value?: string;
 };
 
 export const checkForDuplicateCases = (testCases: Array<Record<string, unknown>>): void => {
@@ -48,187 +49,198 @@ export const checkForDuplicateCases = (testCases: Array<Record<string, unknown>>
 
 export const mainNormalTestCases: Array<MainTestCase> = [
 	{
-		type: 'pre_release',
-		inputVer: '4.10.9.rc.011',
-		outputVer: '4.10.9.beta',
-	},
-	{
-		type: 'pre_release',
-		inputVer: '4.10.9.rc',
-		outputVer: '4.10.9.beta',
-	},
-	{
-		type: 'pre_release',
-		inputVer: '4.10.9.beta',
-		outputVer: '4.10.9.beta',
-	},
-	{
-		type: 'pre_release',
-		inputVer: '0.0.0',
-		outputVer: '0.0.0.beta',
-	},
-	{
-		type: 'decaf',
-		inputVer: '4.10.9.rc.011',
-		outputVer: '4.10.9.decaf',
-	},
-	{
-		type: 'decaf',
-		inputVer: '4.10.9.rc',
-		outputVer: '4.10.9.decaf',
-	},
-	{
-		type: 'decaf',
-		inputVer: '4.10.9.beta',
-		outputVer: '4.10.9.decaf',
-	},
-	{
-		type: 'decaf',
-		inputVer: '0.0.0',
-		outputVer: '0.0.0.decaf',
-	},
-	{
-		type: 'rc',
-		inputVer: '4.10.9.rc.011',
-		// only build shoud be incremented
-		outputVer: '4.10.9.rc.012',
-	},
-	{
-		type: 'rc',
-		inputVer: '4.10.9.rc',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.rc.001',
-	},
-	{
-		type: 'rc',
-		inputVer: '4.10.9.beta',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.rc.001',
-	},
-	{
-		type: 'rc',
-		inputVer: '0.0.0',
-		// patch shoud be incremented and build set to first
-		outputVer: '0.0.1.rc.001',
-	},
-	{
-		type: 'rc',
-		inputVer: '4.10.9.beta.011',
-		// only build shoud be incremented
-		outputVer: '4.10.9.rc.012',
-	},
-	{
-		type: 'alpha',
-		inputVer: '4.10.9.rc.011',
-		// only build shoud be incremented
-		outputVer: '4.10.9.alpha.012',
-	},
-	{
-		type: 'alpha',
-		inputVer: '4.10.9.rc',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.alpha.001',
-	},
-	{
-		type: 'alpha',
-		inputVer: '4.10.9.beta',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.alpha.001',
-	},
-	{
-		type: 'alpha',
-		inputVer: '0.0.0',
-		// patch shoud be incremented and build set to first
-		outputVer: '0.0.1.alpha.001',
-	},
-	{
-		type: 'beta',
-		inputVer: '4.10.9.rc.011',
-		// only build shoud be incremented
-		outputVer: '4.10.9.beta.012',
-	},
-	{
-		type: 'beta',
-		inputVer: '4.10.9.rc',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.beta.001',
-	},
-	{
-		type: 'beta',
-		inputVer: '4.10.9.beta',
-		// patch shoud be incremented and build set to first
-		outputVer: '4.10.10.beta.001',
-	},
-	{
-		type: 'beta',
-		inputVer: '0.0.0',
-		// patch shoud be incremented and build set to first
-		outputVer: '0.0.1.beta.001',
-	},
-	{
-		type: 'minor',
-		inputVer: '4.10.9.rc.011',
-		// minor is incremented and patch is set to 0
-		outputVer: '4.11.0.p',
-	},
-	{
-		type: 'minor',
-		inputVer: '4.10.9.rc',
-		outputVer: '4.11.0.p',
-	},
-	{
-		type: 'minor',
-		inputVer: '4.10.9.beta',
-		outputVer: '4.11.0.p',
-	},
-	{
-		type: 'minor',
-		inputVer: '4.8.0.beta',
-		// no change to patch when it's already 0
-		outputVer: '4.9.0.p',
-	},
-	{
-		type: 'minor',
-		inputVer: '0.0.0',
-		outputVer: '0.1.0.p',
-	},
-	{
 		type: 'major',
 		inputVer: '4.10.9.rc.011',
 		// major is incremented and minor and patch are set to 0
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.rc',
 	},
 	{
 		type: 'major',
 		inputVer: '4.10.9.rc',
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.rc',
 	},
 	{
 		type: 'major',
 		inputVer: '4.10.9.beta',
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.beta',
 	},
 	{
 		type: 'major',
 		inputVer: '4.8.0.beta',
 		// no change to patch when it's already 0
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.beta',
 	},
 	{
 		type: 'major',
 		inputVer: '4.0.8.beta',
 		// no change to minor when it's already 0
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.beta',
 	},
 	{
 		type: 'major',
 		inputVer: '4.0.0.beta',
 		// no change to minor and patch when already 0
-		outputVer: '5.0.0.p',
+		outputVer: '5.0.0.beta',
 	},
 	{
 		type: 'major',
 		inputVer: '0.0.0',
-		outputVer: '1.0.0.p',
+		outputVer: '1.0.0.rc',
+	},
+	{
+		type: 'major',
+		inputVer: '0.0.0',
+		outputVer: '5.0.0.rc',
+		// explicitly set the version
+		value: '5',
+	},
+	{
+		type: 'minor',
+		inputVer: '4.10.9.rc.011',
+		// minor is incremented and patch is set to 0
+		outputVer: '4.11.0.rc',
+	},
+	{
+		type: 'minor',
+		inputVer: '4.10.9.rc',
+		outputVer: '4.11.0.rc',
+	},
+	{
+		type: 'minor',
+		inputVer: '4.3.9.beta',
+		outputVer: '4.4.0.beta',
+	},
+	{
+		type: 'minor',
+		inputVer: '4.8.0.beta',
+		// no change to patch when it's already 0
+		outputVer: '4.9.0.beta',
+	},
+	{
+		type: 'minor',
+		inputVer: '0.0.0',
+		outputVer: '0.1.0.rc',
+	},
+	{
+		type: 'minor',
+		inputVer: '5.3.1',
+		outputVer: '5.5.0.rc',
+		// explicitly set the version
+		value: '5',
+	},
+	{
+		type: 'patch',
+		inputVer: '4.10.9.rc.011',
+		// only patch shoud be incremented and build stripped
+		outputVer: '4.10.10.rc',
+	},
+	{
+		type: 'patch',
+		inputVer: '4.10.9.rc',
+		// only patch shoud be incremented
+		outputVer: '4.10.10.rc',
+	},
+	{
+		type: 'patch',
+		inputVer: '2.10.9.beta',
+		// patch shoud be incremented
+		outputVer: '2.10.10.beta',
+	},
+	{
+		type: 'patch',
+		inputVer: '0.0.0',
+		// patch shoud be incremented
+		outputVer: '0.0.1.rc',
+	},
+	{
+		type: 'patch',
+		inputVer: '5.10.9.beta.011',
+		// patch shoud be incremented and build should be stripped
+		outputVer: '5.10.10.beta',
+	},
+	{
+		type: 'patch',
+		inputVer: '1.0.6',
+		outputVer: '1.0.5.rc',
+		// explicitly set the version
+		value: '5',
+	},
+	{
+		type: 'release_type',
+		inputVer: '3.10.9.rc.011',
+		// no change
+		outputVer: '3.10.9.rc.011',
+	},
+	{
+		type: 'release_type',
+		inputVer: '7.10.9.rc',
+		outputVer: '7.10.9.beta',
+		value: 'beta',
+	},
+	{
+		type: 'release_type',
+		inputVer: '4.10.9.rc',
+		outputVer: '4.10.9.rc',
+		value: 'rc',
+	},
+	{
+		type: 'release_type',
+		inputVer: '8.10.9.p',
+		outputVer: '8.10.9.beta',
+		value: 'beta',
+	},
+	{
+		type: 'release_type',
+		inputVer: '0.0.0',
+		outputVer: '0.0.0.rc',
+	},
+	{
+		type: 'release_type',
+		inputVer: '4.10.9.p',
+		outputVer: '4.10.9.alpha',
+		value: 'alpha',
+	},
+	{
+		type: 'release_type',
+		inputVer: '4.10.8.rc.011',
+		outputVer: '4.10.8.p',
+		value: 'p',
+	},
+	{
+		type: 'build',
+		inputVer: '4.10.9.rc.011',
+		// only build shoud be incremented
+		outputVer: '4.10.9.rc.012',
+	},
+	{
+		type: 'build',
+		inputVer: '4.10.9.rc',
+		// build should be set to first
+		outputVer: '4.10.9.rc.001',
+	},
+	{
+		type: 'build',
+		inputVer: '4.10.2.beta',
+		// build should be set to first
+		outputVer: '4.10.2.beta.001',
+	},
+	{
+		type: 'build',
+		inputVer: '0.0.0',
+		// build should be set to first and release type should be rc by default
+		outputVer: '0.0.0.rc.001',
+	},
+	{
+		type: 'build',
+		inputVer: '4.11.9.beta.011',
+		// build shoud be incremented and build should be stripped
+		outputVer: '4.11.9.beta.012',
+	},
+	{
+		type: 'build',
+		inputVer: '2.4.0',
+		outputVer: '2.4.0.rc.005',
+		// explicitly set the version
+		value: '005',
 	},
 ];
