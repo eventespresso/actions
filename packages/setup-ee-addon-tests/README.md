b93fbcc9a99f9a1c9065329ee194077522d97e56
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-11-25 16:07:13 +0530

    Create install-wp-tests and setup-ee-addon-tests action (#11)
    * Create install-wp-tests action
    
    * Create setup ee addon tests action
    
    * Update actions
    
    * Make scripts executable
    
    * Update scripts to use bash
    
    * Update Dockerfile
    
    * install subversion for script to run
    
    * Update Dockerfile
    
    * use mariadb-client
    
    * use default-mysql-client
    
    * Update Dockerfile
    
    * Switch to composite actions
    
    * Make them executable

---
+++ packages/setup-ee-addon-tests/README.md
@@ -0,0 +1,31 @@
+# `setup-ee-addon-tests` - **Github Action**
+
+This action sets up test env for EE addons
+
+## Input
+
+| Name         | Description                  |
+| ------------ | ---------------------------- |
+| `ee-version` | EE version to use. [=master] |
+
+## Example Workflow File
+
+```yaml
+name: Set up EE addon tests
+
+on:
+    push:
+        branches: [master]
+
+jobs:
+    makepot:
+        runs-on: ubuntu-latest
+        steps:
+            - name: Checkout the commit
+              uses: actions/checkout@v2
+
+            - name: Set up EE addon tests
+              uses: eventespresso/actions/packages/setup-ee-addon-tests@main
+              with:
+                  ee-version: master
+```
---
+++ packages/setup-ee-addon-tests/setup-ee-addon-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+EE_VERSION=${1-master}
+
+if [ -z "$event_espresso_core_dir" ]; then
+    event_espresso_core_dir="/tmp/event-espresso-core"
+fi
+
+# commands taking care of ee core setup
+# receives an argument indicating what branch to checkout.
+function eeCoreSetup {
+    local BRANCH=$1
+    git clone https://github.com/eventespresso/event-espresso-core.git $event_espresso_core_dir
+    cd $event_espresso_core_dir/tests
+    if [ "$BRANCH" = "master" ]; then
+        git checkout master
+    else
+        git fetch --tags
+        git checkout tags/$BRANCH -b $BRANCH
+    fi
+    #back to previous directory
+    cd -
+    echo "Building against EE core" $BRANCH
+}
+
+
+eeCoreSetup $EE_VERSION
