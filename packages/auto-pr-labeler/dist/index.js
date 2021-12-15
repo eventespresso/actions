5979cc6e6b8a1a57f1bb0779ab5fa90d4636b3ea
tn3rb <tn3rb@users.noreply.github.com>
2021-12-15 21:35:36 +0000

    Build JS - gh-actions

--- packages/auto-pr-labeler/dist/index.js
+++ packages/auto-pr-labeler/dist/index.js
@@ -6428,6 +6428,25 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -6437,18 +6456,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__webpack_require__(117));
 const mutations_1 = __webpack_require__(89);
-const core_1 = __importDefault(__webpack_require__(117));
 const queries_1 = __webpack_require__(360);
 const assignStatusLabelsToPullRequest = () => __awaiter(void 0, void 0, void 0, function* () {
-    const org = core_1.default.getInput('org', { required: true }) || 'eventespresso';
-    const repo = core_1.default.getInput('repo', { required: true }) || 'barista';
-    const pr = Number(core_1.default.getInput('pr', { required: true }));
-    const token = core_1.default.getInput('token', { required: true });
+    const org = core.getInput('org', { required: true }) || 'eventespresso';
+    const repo = core.getInput('repo', { required: true }) || 'barista';
+    const pr = Number(core.getInput('pr', { required: true }));
+    const token = core.getInput('token', { required: true });
     // eslint-disable-next-line no-console
     console.log('%c organization', 'color: LimeGreen;', org);
     // eslint-disable-next-line no-console
@@ -6464,7 +6480,7 @@ try {
     assignStatusLabelsToPullRequest();
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 
--- packages/auto-pr-labeler/lib/index.js
+++ packages/auto-pr-labeler/lib/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,18 +27,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
 const mutations_1 = require("./mutations");
-const core_1 = __importDefault(require("@actions/core"));
 const queries_1 = require("./queries");
 const assignStatusLabelsToPullRequest = () => __awaiter(void 0, void 0, void 0, function* () {
-    const org = core_1.default.getInput('org', { required: true }) || 'eventespresso';
-    const repo = core_1.default.getInput('repo', { required: true }) || 'barista';
-    const pr = Number(core_1.default.getInput('pr', { required: true }));
-    const token = core_1.default.getInput('token', { required: true });
+    const org = core.getInput('org', { required: true }) || 'eventespresso';
+    const repo = core.getInput('repo', { required: true }) || 'barista';
+    const pr = Number(core.getInput('pr', { required: true }));
+    const token = core.getInput('token', { required: true });
     // eslint-disable-next-line no-console
     console.log('%c organization', 'color: LimeGreen;', org);
     // eslint-disable-next-line no-console
@@ -35,5 +51,5 @@ try {
     assignStatusLabelsToPullRequest();
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
