729a2161a688d0a33982e2781041cb7de03117e7
Brent Christensen <brent@eventespresso.com>
2021-12-14 19:13:56 -0800

    define the types we'll need

---
+++ packages/auto-pr-labeler/src/types.ts
@@ -0,0 +1,23 @@
+export interface Label {
+	name: string;
+	id: string;
+}
+
+export interface LabelList {
+	[key: string]: Label;
+}
+
+export interface LabelsQueryResponse {
+	labels: Label;
+}
+
+export interface PullRequest {
+	id: string;
+	number: number;
+	reviewDecision: 'APPROVED' | 'CHANGES_REQUESTED' | 'REVIEW_REQUIRED' | null;
+	state: 'CLOSED' | 'MERGED' | 'OPEN';
+}
+
+export interface PullRequestQueryResponse {
+	pullRequest: PullRequest;
+}
