826291c4abc6719727bc604ca67c5af60f20d9bf
Brent Christensen <brent@eventespresso.com>
2021-12-15 14:03:24 -0800

    add try catch inside assignStatusLabels()

--- packages/auto-pr-labeler/src/index.ts
+++ packages/auto-pr-labeler/src/index.ts
@@ -2,27 +2,27 @@ import * as core from '@actions/core';
 import { assignStatusLabels } from './mutations';
 import { getPullRequest } from './queries';
 
-const assignStatusLabelsToPullRequest = async (): Promise<void> => {
-	const org = core.getInput('org', { required: true }) || 'eventespresso';
-	const repo = core.getInput('repo', { required: true }) || 'barista';
-	const pr = Number(core.getInput('pr', { required: true }));
-	const token = core.getInput('token', { required: true });
-
-	// eslint-disable-next-line no-console
-	console.log('%c organization', 'color: LimeGreen;', org);
-	// eslint-disable-next-line no-console
-	console.log('%c repository', 'color: Yellow;', pr);
-	// eslint-disable-next-line no-console
-	console.log('%c pull request #', 'color: HotPink;', pr);
-
-	const { pullRequest } = await getPullRequest(org, repo, pr, token);
-
-	if (pullRequest) {
-		assignStatusLabels(pullRequest, token);
-	}
-};
-
 try {
+	const assignStatusLabelsToPullRequest = async (): Promise<void> => {
+		const org = core.getInput('org', { required: true }) || 'eventespresso';
+		const repo = core.getInput('repo', { required: true }) || 'barista';
+		const pr = Number(core.getInput('pr', { required: true }));
+		const token = core.getInput('token', { required: true });
+
+		// eslint-disable-next-line no-console
+		console.log('%c organization', 'color: LimeGreen;', org);
+		// eslint-disable-next-line no-console
+		console.log('%c repository', 'color: Yellow;', pr);
+		// eslint-disable-next-line no-console
+		console.log('%c pull request #', 'color: HotPink;', pr);
+
+		const { pullRequest } = await getPullRequest(org, repo, pr, token);
+
+		if (pullRequest) {
+			assignStatusLabels(pullRequest, token);
+		}
+	};
+
 	assignStatusLabelsToPullRequest();
 } catch (error) {
 	core.setFailed(error.message);
