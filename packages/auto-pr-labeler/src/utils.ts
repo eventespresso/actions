1bf53fb934de46523ddfcf60437cfc594a29738a
Brent Christensen <brent@eventespresso.com>
2021-12-15 17:08:03 -0800

    improve/fix headers

--- packages/auto-pr-labeler/src/utils.ts
+++ packages/auto-pr-labeler/src/utils.ts
@@ -1,9 +1,18 @@
+import * as core from '@actions/core';
 import { graphql } from '@octokit/graphql';
-import type { graphql as iGraphql } from '@octokit/graphql/dist-types/types';
 
-export const graphqlWithAuth = (token: string): iGraphql =>
-	graphql.defaults({
-		headers: {
-			authorization: token,
-		},
-	});
+const owner = core.getInput('org', { required: true });
+const repo = core.getInput('repo', { required: true });
+const token = core.getInput('token', { required: true });
+
+// eslint-disable-next-line no-console
+console.log('%c organization', 'color: LimeGreen;', owner);
+// eslint-disable-next-line no-console
+console.log('%c repository', 'color: Yellow;', repo);
+
+const headers = {
+	'Content-Type': 'application/json',
+	authorization: `Bearer ${token}`,
+};
+// ex: {"owner": "eventespresso", "repo":"barista"}
+export const graphqlWithAuth = graphql.defaults({ owner, repo, headers });
