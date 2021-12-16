108c33fb91f8d67df291e2cab9a04aa4c91fd54e
Brent Christensen <brent@eventespresso.com>
2021-12-16 10:50:17 -0800

    import pr # from utils

--- packages/auto-pr-labeler/src/index.ts
+++ packages/auto-pr-labeler/src/index.ts
@@ -1,12 +1,9 @@
 import * as core from '@actions/core';
 import { assignStatusLabels } from './mutations';
 import { getPullRequest } from './queries';
+import { pr } from './utils';
 
 const assignStatusLabelsToPullRequest = async (): Promise<void> => {
-	const pr = Number(core.getInput('prNumber', { required: true }));
-	// eslint-disable-next-line no-console
-	console.log('%c pull request #', 'color: HotPink;', pr);
-
 	const { pullRequest } = await getPullRequest(pr);
 	// eslint-disable-next-line no-console
 	console.log('%c pull request', 'color: cyan;', pullRequest);
--- packages/auto-pr-labeler/src/utils.ts
+++ packages/auto-pr-labeler/src/utils.ts
@@ -15,11 +15,14 @@ console.log('%c ownerRepo', 'color: LimeGreen;', ownerRepo);
 const owner = core.getInput('owner', { required: true });
 const repo = core.getInput('repo', { required: true });
 const token = core.getInput('token', { required: true });
+export const pr = Number(core.getInput('prNumber', { required: true }));
 
 // eslint-disable-next-line no-console
 console.log('%c organization', 'color: LimeGreen;', owner);
 // eslint-disable-next-line no-console
 console.log('%c repository', 'color: Yellow;', repo);
+// eslint-disable-next-line no-console
+console.log('%c pull request #', 'color: HotPink;', pr);
 
 const headers = {
 	'Content-Type': 'application/json',
