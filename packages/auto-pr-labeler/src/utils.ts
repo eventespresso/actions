f4424e77a5c38a3e058d793cf6737f18ff0039e6
Brent Christensen <brent@eventespresso.com>
2021-12-16 10:07:15 -0800

    try grabbing env vars

--- packages/auto-pr-labeler/src/utils.ts
+++ packages/auto-pr-labeler/src/utils.ts
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
 
+const eventPayload = require(process?.env?.GITHUB_EVENT_PATH);
+const ownerRepo = require(process?.env?.GITHUB_REPOSITORY);
+console.log('%c eventPayload?.owner', 'color: LimeGreen;', eventPayload?.owner);
+console.log('%c eventPayload?.repo', 'color: LimeGreen;', eventPayload?.repo);
+console.log('%c ownerRepo', 'color: LimeGreen;', ownerRepo);
 const owner = core.getInput('owner', { required: true });
 const repo = core.getInput('repo', { required: true });
 const token = core.getInput('token', { required: true });
