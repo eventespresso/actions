801034adace60714ca376e202982c003e81978c7
Brent Christensen <brent@eventespresso.com>
2021-12-15 18:02:56 -0800

    DONT Use try-catch Everywhere! (#23)

--- packages/auto-pr-labeler/src/index.ts
+++ packages/auto-pr-labeler/src/index.ts
@@ -2,21 +2,21 @@ import * as core from '@actions/core';
 import { assignStatusLabels } from './mutations';
 import { getPullRequest } from './queries';
 
+const assignStatusLabelsToPullRequest = async (): Promise<void> => {
+	const pr = Number(core.getInput('prNumber', { required: true }));
+	// eslint-disable-next-line no-console
+	console.log('%c pull request #', 'color: HotPink;', pr);
+
+	const { pullRequest } = await getPullRequest(pr);
+	// eslint-disable-next-line no-console
+	console.log('%c pull request', 'color: cyan;', pullRequest);
+
+	if (pullRequest) {
+		assignStatusLabels(pullRequest);
+	}
+};
+
 try {
-	const assignStatusLabelsToPullRequest = async (): Promise<void> => {
-		const pr = Number(core.getInput('prNumber', { required: true }));
-		// eslint-disable-next-line no-console
-		console.log('%c pull request #', 'color: HotPink;', pr);
-
-		const { pullRequest } = await getPullRequest(pr);
-		// eslint-disable-next-line no-console
-		console.log('%c pull request', 'color: cyan;', pullRequest);
-
-		if (pullRequest) {
-			assignStatusLabels(pullRequest);
-		}
-	};
-
 	assignStatusLabelsToPullRequest();
 } catch (error) {
 	core.setFailed(error.message);
--- packages/auto-pr-labeler/src/queries.ts
+++ packages/auto-pr-labeler/src/queries.ts
@@ -1,57 +1,48 @@
-import * as core from '@actions/core';
 import type { LabelsQueryResponse, PullRequestQueryResponse } from './types';
 import { graphqlWithAuth } from './utils';
 
-export const getLabels = async (): Promise<LabelsQueryResponse> => {
-	try {
-		return await graphqlWithAuth(
-			`
-			query ($owner: String!, $repo: String!) {
+export const getLabels = (): Promise<LabelsQueryResponse> => {
+	return graphqlWithAuth(
+		`
+		query ($owner: String!, $repo: String!) {
+			repository(name: $repo, owner: $owner) {
+				labels(first: 100, orderBy: {direction:ASC, field: NAME}) {
+					nodes {
+					name
+					id
+					}
+				}
+			}
+		}
+	`
+	);
+};
+
+export const getPullRequest = (pr: number): Promise<PullRequestQueryResponse> => {
+	return graphqlWithAuth(
+		`
+			query ($pr: Int!, $owner: String!, $repo: String!) {
 				repository(name: $repo, owner: $owner) {
-					labels(first: 100, orderBy: {direction:ASC, field: NAME}) {
-						nodes {
-						name
+					pullRequest(number: $pr) {
 						id
+						labels(first: 10) {
+							nodes {
+								name
+							}
+						}
+						number
+						reviewDecision
+						state
+						closingIssuesReferences(first: 10) {
+							nodes {
+								id
+								number
+							}
 						}
 					}
 				}
 			}
-		`
-		);
-	} catch (error) {
-		core.setFailed(error.message);
-	}
-};
-
-export const getPullRequest = async (pr: number): Promise<PullRequestQueryResponse> => {
-	try {
-		return await graphqlWithAuth(
-			`
-				query ($pr: Int!, $owner: String!, $repo: String!) {
-					repository(name: $repo, owner: $owner) {
-						pullRequest(number: $pr) {
-							id
-							labels(first: 10) {
-								nodes {
-									name
-								}
-							}
-							number
-							reviewDecision
-							state
-							closingIssuesReferences(first: 10) {
-								nodes {
-									id
-									number
-								}
-							}
-						}
-					}
-				}
-			`,
-			{ pr }
-		);
-	} catch (error) {
-		core.setFailed(error.message);
-	}
+		`,
+		{ pr }
+	);
 };
