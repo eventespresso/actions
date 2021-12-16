10c5dfb7a42eb19af9db8c762f8649dbdc781494
Brent Christensen <brent@eventespresso.com>
2021-12-15 17:38:33 -0800

    use try-catch everywhere

--- packages/auto-pr-labeler/src/queries.ts
+++ packages/auto-pr-labeler/src/queries.ts
@@ -1,48 +1,57 @@
+import * as core from '@actions/core';
 import type { LabelsQueryResponse, PullRequestQueryResponse } from './types';
 import { graphqlWithAuth } from './utils';
 
 export const getLabels = async (): Promise<LabelsQueryResponse> => {
-	return await graphqlWithAuth(
-		`
-		query ($owner: String!, $repo: String!) {
-			repository(name: $repo, owner: $owner) {
-				labels(first: 100, orderBy: {direction:ASC, field: NAME}) {
-					nodes {
-					name
-					id
+	try {
+		return await graphqlWithAuth(
+			`
+			query ($owner: String!, $repo: String!) {
+				repository(name: $repo, owner: $owner) {
+					labels(first: 100, orderBy: {direction:ASC, field: NAME}) {
+						nodes {
+						name
+						id
+						}
 					}
 				}
 			}
-		}
-	`
-	);
+		`
+		);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 };
 
 export const getPullRequest = async (pr: number): Promise<PullRequestQueryResponse> => {
-	return await graphqlWithAuth(
-		`
-			query ($pr: Int!, $owner: String!, $repo: String!) {
-				repository(name: $repo, owner: $owner) {
-					pullRequest(number: $pr) {
-						id
-						labels(first: 10) {
-							nodes {
-								name
+	try {
+		return await graphqlWithAuth(
+			`
+				query ($pr: Int!, $owner: String!, $repo: String!) {
+					repository(name: $repo, owner: $owner) {
+						pullRequest(number: $pr) {
+							id
+							labels(first: 10) {
+								nodes {
+									name
+								}
 							}
-						}
-						number
-						reviewDecision
-						state
-						closingIssuesReferences(first: 10) {
-							nodes {
-								id
-								number
+							number
+							reviewDecision
+							state
+							closingIssuesReferences(first: 10) {
+								nodes {
+									id
+									number
+								}
 							}
 						}
 					}
 				}
-			}
-		`,
-		{ pr }
-	);
+			`,
+			{ pr }
+		);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 };
