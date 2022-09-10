8416915ffea765c07321b728195882ad14db92c9
Brent Christensen <brent@eventespresso.com>
2022-09-09 17:40:01 -0700

    labels are repo specific DUH!!!

--- packages/auto-pr-labeler/src/labels.ts
+++ packages/auto-pr-labeler/src/labels.ts
@@ -1,176 +1,352 @@
-import type { LabelList } from './types';
+import type { RepoLabels } from './types';
 
-export const labels: LabelList = {
-	catUiUx: {
-		name: 'C: UI/UX 🚽',
-		id: 'MDU6TGFiZWwyMjExNDkyNTUz',
+export const repoLabels: RepoLabels = {
+	barista: {
+		catUiUx: {
+			name: 'C: UI/UX 🚽',
+			id: 'MDU6TGFiZWwyMjExNDkyNTUz',
+		},
+		catAccessibility: {
+			name: 'C: accessibility ♿',
+			id: 'MDU6TGFiZWwyMzcwMzc2Njkz',
+		},
+		catAssets: {
+			name: 'C: assets 💎',
+			id: 'MDU6TGFiZWwyMjExNTM1MjEx',
+		},
+		catAutoDeploy: {
+			name: 'C: automation & deployment ⚙️',
+			id: 'MDU6TGFiZWwyMjExNTc1ODQx',
+		},
+		catBuildProcess: {
+			name: 'C: build process 🔨',
+			id: 'MDU6TGFiZWw4NzA2NjYyMTg=',
+		},
+		catDataSystems: {
+			name: 'C: data systems 🗑',
+			id: 'MDU6TGFiZWwyMjExNTIyNDgx',
+		},
+		catDocs: {
+			name: 'C: documentation 📑',
+			id: 'MDU6TGFiZWwyNDY3NzEwOTE1',
+		},
+		catServices: {
+			name: 'C: services 🤝',
+			id: 'MDU6TGFiZWwyMjExNjExNDgx',
+		},
+		catTesting: {
+			name: 'C: testing 🧪',
+			id: 'MDU6TGFiZWwyMzE2NTE1ODg4',
+		},
+		domainEdtr: {
+			name: 'D: EDTR ✏️',
+			id: 'MDU6TGFiZWwyMjIxMDY4OTI4',
+		},
+		domainCore: {
+			name: 'D: EE CORE ☕',
+			id: 'MDU6TGFiZWwyMTkzMTE4NTgx',
+		},
+		domainEventSmart: {
+			name: 'D: Event Smart 🛒',
+			id: 'MDU6TGFiZWwyNzE5MjI4ODkz',
+		},
+		domainBlocks: {
+			name: 'D: GB Blocks 🧱',
+			id: 'MDU6TGFiZWwyMzU5MDQ0MTkz',
+		},
+		domainPackages: {
+			name: 'D: Packages 📦',
+			id: 'MDU6TGFiZWwyMjUzODIzNjUy',
+		},
+		domainRemAddon: {
+			name: 'D: REM ♺',
+			id: 'MDU6TGFiZWwyMTY4NjQzMjQz',
+		},
+		domainRepo: {
+			name: 'D: REPO 💾',
+			id: 'LA_kwDOB32LW87Y7L-j',
+		},
+		domainWpUserAddon: {
+			name: 'D: WP User Add-on 👤',
+			id: 'MDU6TGFiZWwyNTY4MDE2MDkx',
+		},
+		domainWpAdmin: {
+			name: 'D: WP admin 🏠',
+			id: 'MDU6TGFiZWwyMjExNjI2NTEy',
+		},
+		priorityUrgent: {
+			name: 'P1: URGENT PRIORITY :scream:',
+			id: 'MDU6TGFiZWwyMjc0MDkzMDA0',
+		},
+		priorityHigh: {
+			name: 'P2: HIGH priority 😮',
+			id: 'MDU6TGFiZWwyMjc0MDkxMjE2',
+		},
+		priorityMedium: {
+			name: 'P3: med priority 😐',
+			id: 'MDU6TGFiZWwyMjc0MDgzMTE1',
+		},
+		priorityLow: {
+			name: 'P4: low priority 😎',
+			id: 'MDU6TGFiZWwyMjc0MDc0NjY5',
+		},
+		statusNew: {
+			name: 'S:1 new 👶🏻',
+			id: 'MDU6TGFiZWwyNDQ0NjI1MDU2',
+		},
+		statusPlanning: {
+			name: 'S2: planning 📋',
+			id: 'MDU6TGFiZWwyMjExNDYwNjA5',
+		},
+		statusNeedsFeedback: {
+			name: 'S3: needs feedback ❔',
+			id: 'MDU6TGFiZWwyMzY3Mjc0Nzkw',
+		},
+		statusInProgress: {
+			name: 'S4: in progress 🚧',
+			id: 'MDU6TGFiZWwyMjExODIxMTUx',
+		},
+		statusCodeReview: {
+			name: 'S5: code review 🔎',
+			id: 'MDU6TGFiZWwyMTYxMzQzNTg5',
+		},
+		statusPleaseFix: {
+			name: 'S6: please fix 💉',
+			id: 'MDU6TGFiZWwyMjExNTA0MTAx',
+		},
+		statusApproved: {
+			name: 'S:7 APPROVED ✔️',
+			id: 'MDU6TGFiZWwyNTQ4NjEyMDU3',
+		},
+		statusNeedsTesting: {
+			name: 'S8: needs testing 🧪',
+			id: 'MDU6TGFiZWw5MDYyNjIwMTI=',
+		},
+		statusCompleted: {
+			name: 'S9: completed 🚀',
+			id: 'MDU6TGFiZWw4NzA2NjYyMTc=',
+		},
+		statusBlocked: {
+			name: 's10: BLOCKED ⚠️',
+			id: 'MDU6TGFiZWwyNDA1MzMwNjkw',
+		},
+		statusDuplicate: {
+			name: 'S11: duplicate? ©️',
+			id: 'MDU6TGFiZWw4NzA2NjYyMTU=',
+		},
+		statusInvalid: {
+			name: 'S12: invalid 👽',
+			id: 'MDU6TGFiZWw4NzA2NjYyMjE=',
+		},
+		typeBug: {
+			name: 'T: bug 🐞',
+			id: 'MDU6TGFiZWw4NzA2NjYyMTQ=',
+		},
+		typeCodeQuality: {
+			name: 'T: code quality 👾',
+			id: 'MDU6TGFiZWwyMzcyNTUxOTM1',
+		},
+		typeEnhancement: {
+			name: 'T: enhancement ✨',
+			id: 'MDU6TGFiZWw4NzA2NjYyMTY=',
+		},
+		typeNewFeature: {
+			name: 'T: new feature 🎁',
+			id: 'MDU6TGFiZWwyMjExNTQ1NDg5',
+		},
+		typeTask: {
+			name: 'T: task 🧹',
+			id: 'MDU6TGFiZWw4NzA2NjYyMjA=',
+		},
+		xEval: {
+			name: 'X: EVALUATION-ISSUE 🎯',
+			id: 'MDU6TGFiZWwzMDY3NTA0NDcy',
+		},
+		xEzPz: {
+			name: 'X: EZ PZ 🍰',
+			id: 'MDU6TGFiZWwzMjE1MjMzMDAy',
+		},
+		xHard: {
+			name: 'X: HARD ☠️',
+			id: 'MDU6TGFiZWwzMjE1MjA5Nzk3',
+		},
+		xDependencies: {
+			name: 'dependencies',
+			id: 'LA_kwDOB32LW87RudBy',
+		},
 	},
-	catAccessibility: {
-		name: 'C: accessibility ♿',
-		id: 'MDU6TGFiZWwyMzcwMzc2Njkz',
-	},
-	catAssets: {
-		name: 'C: assets 💎',
-		id: 'MDU6TGFiZWwyMjExNTM1MjEx',
-	},
-	catAutoDeploy: {
-		name: 'C: automation & deployment ⚙️',
-		id: 'MDU6TGFiZWwyMjExNTc1ODQx',
-	},
-	catBuildProcess: {
-		name: 'C: build process 🔨',
-		id: 'MDU6TGFiZWw4NzA2NjYyMTg=',
-	},
-	catDataSystems: {
-		name: 'C: data systems 🗑',
-		id: 'MDU6TGFiZWwyMjExNTIyNDgx',
-	},
-	catDocs: {
-		name: 'C: documentation 📑',
-		id: 'MDU6TGFiZWwyNDY3NzEwOTE1',
-	},
-	catServices: {
-		name: 'C: services 🤝',
-		id: 'MDU6TGFiZWwyMjExNjExNDgx',
-	},
-	catTesting: {
-		name: 'C: testing 🧪',
-		id: 'MDU6TGFiZWwyMzE2NTE1ODg4',
-	},
-	domainEdtr: {
-		name: 'D: EDTR ✏️',
-		id: 'MDU6TGFiZWwyMjIxMDY4OTI4',
-	},
-	domainCore: {
-		name: 'D: EE CORE ☕',
-		id: 'MDU6TGFiZWwyMTkzMTE4NTgx',
-	},
-	domainEventSmart: {
-		name: 'D: Event Smart 🛒',
-		id: 'MDU6TGFiZWwyNzE5MjI4ODkz',
-	},
-	domainBlocks: {
-		name: 'D: GB Blocks 🧱',
-		id: 'MDU6TGFiZWwyMzU5MDQ0MTkz',
-	},
-	domainPackages: {
-		name: 'D: Packages 📦',
-		id: 'MDU6TGFiZWwyMjUzODIzNjUy',
-	},
-	domainRemAddon: {
-		name: 'D: REM ♺',
-		id: 'MDU6TGFiZWwyMTY4NjQzMjQz',
-	},
-	domainRepo: {
-		name: 'D: REPO 💾',
-		id: 'LA_kwDOB32LW87Y7L-j',
-	},
-	domainWpUserAddon: {
-		name: 'D: WP User Add-on 👤',
-		id: 'MDU6TGFiZWwyNTY4MDE2MDkx',
-	},
-	domainWpAdmin: {
-		name: 'D: WP admin 🏠',
-		id: 'MDU6TGFiZWwyMjExNjI2NTEy',
-	},
-	priorityUrgent: {
-		name: 'P1: URGENT PRIORITY :scream:',
-		id: 'MDU6TGFiZWwyMjc0MDkzMDA0',
-	},
-	priorityHigh: {
-		name: 'P2: HIGH priority 😮',
-		id: 'MDU6TGFiZWwyMjc0MDkxMjE2',
-	},
-	priorityMedium: {
-		name: 'P3: med priority 😐',
-		id: 'MDU6TGFiZWwyMjc0MDgzMTE1',
-	},
-	priorityLow: {
-		name: 'P4: low priority 😎',
-		id: 'MDU6TGFiZWwyMjc0MDc0NjY5',
-	},
-	statusNew: {
-		name: 'S:1 new 👶🏻',
-		id: 'MDU6TGFiZWwyNDQ0NjI1MDU2',
-	},
-	statusPlanning: {
-		name: 'S2: planning 📋',
-		id: 'MDU6TGFiZWwyMjExNDYwNjA5',
-	},
-	statusNeedsFeedback: {
-		name: 'S3: needs feedback ❔',
-		id: 'MDU6TGFiZWwyMzY3Mjc0Nzkw',
-	},
-	statusInProgress: {
-		name: 'S4: in progress 🚧',
-		id: 'MDU6TGFiZWwyMjExODIxMTUx',
-	},
-	statusCodeReview: {
-		name: 'S5: code review 🔎',
-		id: 'MDU6TGFiZWwyMTYxMzQzNTg5',
-	},
-	statusPleaseFix: {
-		name: 'S6: please fix 💉',
-		id: 'MDU6TGFiZWwyMjExNTA0MTAx',
-	},
-	statusApproved: {
-		name: 'S:7 APPROVED ✔️',
-		id: 'MDU6TGFiZWwyNTQ4NjEyMDU3',
-	},
-	statusNeedsTesting: {
-		name: 'S8: needs testing 🧪',
-		id: 'MDU6TGFiZWw5MDYyNjIwMTI=',
-	},
-	statusCompleted: {
-		name: 'S9: completed 🚀',
-		id: 'MDU6TGFiZWw4NzA2NjYyMTc=',
-	},
-	statusBlocked: {
-		name: 's10: BLOCKED ⚠️',
-		id: 'MDU6TGFiZWwyNDA1MzMwNjkw',
-	},
-	statusDuplicate: {
-		name: 'S11: duplicate? ©️',
-		id: 'MDU6TGFiZWw4NzA2NjYyMTU=',
-	},
-	statusInvalid: {
-		name: 'S12: invalid 👽',
-		id: 'MDU6TGFiZWw4NzA2NjYyMjE=',
-	},
-	typeBug: {
-		name: 'T: bug 🐞',
-		id: 'MDU6TGFiZWw4NzA2NjYyMTQ=',
-	},
-	typeCodeQuality: {
-		name: 'T: code quality 👾',
-		id: 'MDU6TGFiZWwyMzcyNTUxOTM1',
-	},
-	typeEnhancement: {
-		name: 'T: enhancement ✨',
-		id: 'MDU6TGFiZWw4NzA2NjYyMTY=',
-	},
-	typeNewFeature: {
-		name: 'T: new feature 🎁',
-		id: 'MDU6TGFiZWwyMjExNTQ1NDg5',
-	},
-	typeTask: {
-		name: 'T: task 🧹',
-		id: 'MDU6TGFiZWw4NzA2NjYyMjA=',
-	},
-	xEval: {
-		name: 'X: EVALUATION-ISSUE 🎯',
-		id: 'MDU6TGFiZWwzMDY3NTA0NDcy',
-	},
-	xEzPz: {
-		name: 'X: EZ PZ 🍰',
-		id: 'MDU6TGFiZWwzMjE1MjMzMDAy',
-	},
-	xHard: {
-		name: 'X: HARD ☠️',
-		id: 'MDU6TGFiZWwzMjE1MjA5Nzk3',
-	},
-	xDependencies: {
-		name: 'dependencies',
-		id: 'LA_kwDOB32LW87RudBy',
-	},
+	cafe: {
+		catUiUx: {
+			"name": "C: UI/UX 🚽",
+			"id": "LA_kwDOH9Ray88AAAABDP4WVA"
+		},
+		catAssets: {
+			"name": "C: assets 💎",
+			"id": "LA_kwDOH9Ray88AAAABDP38KA"
+		},
+		catAutoDeploy: {
+			"name": "C: automation & deployment ⚙️",
+			"id": "LA_kwDOH9Ray88AAAABDP3_-w"
+		},
+		catDataSystems: {
+			"name": "C: data systems 🗑",
+			"id": "LA_kwDOH9Ray88AAAABDP4HGg"
+		},
+		catBuildProcess: {
+			name: 'C: build process 🔨',
+			id: 'MDU6TGFiWw4NzA2NjYyMTg=',
+		},
+		catDocs: {
+			"name": "C: documentation 📑",
+			"id": "LA_kwDOH9Ray88AAAABDP4KnA"
+		},
+		catI18n: {
+			"name": "C: i18n🌍",
+			"id": "LA_kwDOH9Ray88AAAABDP32qg"
+		},
+		catServices: {
+			"name": "C: services 🤝",
+			"id": "LA_kwDOH9Ray88AAAABDQPW5A"
+		},
+		catTesting: {
+			"name": "C: tests 🧪",
+			"id": "LA_kwDOH9Ray88AAAABDP4R1g"
+		},
+		domainApi: {
+			"name": "D: API📡",
+			"id": "LA_kwDOH9Ray88AAAABDQIsMw"
+		},
+		domainCalendar: {
+			"name": "D: Calendar 📆",
+			"id": "LA_kwDOH9Ray88AAAABDP-zJQ"
+		},
+		domainCore: {
+			"name": "D: EE CORE ☕",
+			"id": "LA_kwDOH9Ray88AAAABDP4q-w"
+		},
+		domainEventSmart: {
+			"name": "D: Event Smart 🛒",
+			"id": "LA_kwDOH9Ray88AAAABDP4xWQ"
+		},
+		domainGateways: {
+			"name": "D: Gateways💲",
+			"id": "LA_kwDOH9Ray88AAAABDP6Fng"
+		},
+		domainMessages: {
+			"name": "D: Messages 📩",
+			"id": "LA_kwDOH9Ray88AAAABDP728Q"
+		},
+		domainRemAddon: {
+			"name": "D: REM ♻️",
+			"id": "LA_kwDOH9Ray88AAAABDP485A"
+		},
+		domainTemplates: {
+			"name": "D: Templates 📰",
+			"id": "LA_kwDOH9Ray88AAAABDQNrGw"
+		},
+		domainThemes: {
+			"name": "D: Themes 🎨",
+			"id": "LA_kwDOH9Ray88AAAABDP8rrA"
+		},
+		domainWpUserAddon: {
+			"name": "D: WP User Add-on 🤷‍♀️",
+			"id": "LA_kwDOH9Ray88AAAABDP5LcA"
+		},
+		priorityUrgent: {
+			"name": "P1: URGENT PRIORITY :scream:",
+			"id": "LA_kwDOH9Ray88AAAABDP9VMg"
+		},
+		priorityHigh: {
+			"name": "P2: HIGH priority 😮",
+			"id": "LA_kwDOH9Ray88AAAABDP9ZRA"
+		},
+		priorityMedium: {
+			"name": "P3: med priority 😐",
+			"id": "LA_kwDOH9Ray88AAAABDP9dew"
+		},
+		priorityLow: {
+			"name": "P4: low priority 😎",
+			"id": "LA_kwDOH9Ray88AAAABDP9iPg"
+		},
+		statusNew: {
+			"name": "S1: new 👶🏻",
+			"id": "LA_kwDOH9Ray88AAAABDP_CWg"
+		},
+		statusPlanning: {
+			"name": "S2: planning 📋",
+			"id": "LA_kwDOH9Ray88AAAABDP_G4w"
+		},
+		statusNeedsFeedback: {
+			"name": "S3: needs feedback ❔",
+			"id": "LA_kwDOH9Ray88AAAABDP_KHg"
+		},
+		statusInProgress: {
+			"name": "S4: in progress 🚧",
+			"id": "LA_kwDOH9Ray88AAAABDP_NKg"
+		},
+		statusCodeReview: {
+			"name": "S5: code review 🔎",
+			"id": "LA_kwDOH9Ray88AAAABDQBBDQ"
+		},
+		statusPleaseFix: {
+			"name": "S6: please fix 💉",
+			"id": "LA_kwDOH9Ray88AAAABDQBERQ"
+		},
+		statusApproved: {
+			"name": "S7: APPROVED ✔️",
+			"id": "LA_kwDOH9Ray88AAAABDQBINw"
+		},
+		statusNeedsTesting: {
+			"name": "S8: needs testing 🧪",
+			"id": "LA_kwDOH9Ray88AAAABDQBKvw"
+		},
+		statusCompleted: {
+			"name": "S9: completed 🚀",
+			"id": "LA_kwDOH9Ray88AAAABDQBQKA"
+		},
+		statusBlocked: {
+			"name": "s10: BLOCKED ⚠️",
+			"id": "LA_kwDOH9Ray88AAAABDQBUYw"
+		},
+		statusDuplicate: {
+			"name": "S11: duplicate? ©️",
+			"id": "LA_kwDOH9Ray88AAAABDQBYiQ"
+		},
+		statusInvalid: {
+			"name": "S12: invalid 👽",
+			"id": "LA_kwDOH9Ray88AAAABDQBcQg"
+		},
+		typeBug: {
+			"name": "T: bug 🪲",
+			"id": "LA_kwDOH9Ray88AAAABDQBr1Q"
+		},
+		typeCodeQuality: {
+			"name": "T: code quality 👾",
+			"id": "LA_kwDOH9Ray88AAAABDQBvig"
+		},
+		typeEnhancement: {
+			"name": "T: enhancement ✨",
+			"id": "LA_kwDOH9Ray88AAAABDQBztw"
+		},
+		typeNewFeature: {
+			"name": "T: new feature 🎁",
+			"id": "LA_kwDOH9Ray88AAAABDQB2fw"
+		},
+		typeTask: {
+			"name": "T: task 🧹",
+			"id": "LA_kwDOH9Ray88AAAABDQB4wA"
+		},
+		xEval: {
+			"name": "X: EVALUATION-ISSUE 🎯",
+			"id": "LA_kwDOH9Ray88AAAABDQB6yA"
+		},
+		xEzPz: {
+			"name": "X: EZ PZ 🍰",
+			"id": "LA_kwDOH9Ray88AAAABDQB96A"
+		},
+		xHard: {
+			"name": "X: HARD ☠️",
+			"id": "LA_kwDOH9Ray88AAAABDQCBDg"
+		},
+	}
 };
