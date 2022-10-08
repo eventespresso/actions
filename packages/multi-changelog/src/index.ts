181fb37e1dabd269691304a02502892a4fc2e661
Brent Christensen <brent@eventespresso.com>
2022-10-07 20:19:31 -0700

    fix linting errors (but not sort???)

--- packages/multi-changelog/src/index.ts
+++ packages/multi-changelog/src/index.ts
@@ -4,7 +4,6 @@ import { existsSync, readFileSync, writeFileSync } from '@eventespresso-actions/
 
 (async () => {
 	try {
-
 		const PAYLOAD = github.context?.payload;
 		// if (PAYLOAD.action !== 'closed' && PAYLOAD.pull_request.merged !== true) {
 		// 	return;
@@ -13,30 +12,30 @@ import { existsSync, readFileSync, writeFileSync } from '@eventespresso-actions/
 		const repo = github.context?.repo?.repo;
 		const owner = github.context?.repo?.owner;
 		if (!repo || !owner) {
-			core.setFailed(`WUT?!?! The repo and/or owner data is missing from the payload?!?!? `)
+			core.setFailed(`WUT?!?! The repo and/or owner data is missing from the payload?!?!? `);
 		}
 
 		const pullRequest = PAYLOAD.pull_request;
-        const base = pullRequest.base?.sha
-		const head = pullRequest.head?.sha
+		const base = pullRequest.base?.sha;
+		const head = pullRequest.head?.sha;
 		if (!base || !head) {
-			core.setFailed(`OH NOES?!?! The base and head commits are missing from the payload?!?!? `)
+			core.setFailed(`OH NOES?!?! The base and head commits are missing from the payload?!?!? `);
 		}
 
-		const GITHUB_TOKEN = core.getInput('token', {required: true});
+		const GITHUB_TOKEN = core.getInput('token', { required: true });
 		const octokit = github.getOctokit(GITHUB_TOKEN);
 
 		// https://developer.github.com/v3/repos/commits/#compare-two-commits
 		const comparison = await octokit.repos.compareCommits({ base, head, owner, repo });
 
 		if (comparison?.status !== 200) {
-			core.setFailed(`GitHub.repos.compareCommits() failed for commits base: ${base} and head: ${head}`)
+			core.setFailed(`GitHub.repos.compareCommits() failed for commits base: ${base} and head: ${head}`);
 		}
 
 		if (comparison.data.status !== 'ahead') {
-			core.setFailed('The BASE is ahead of HEAD but HEAD should be ahead of BASE!?!?.')
+			core.setFailed('The BASE is ahead of HEAD but HEAD should be ahead of BASE!?!?.');
 		}
-		const files = comparison.data.files
+		const files = comparison.data.files;
 
 		const plugins = [];
 
@@ -46,39 +45,37 @@ import { existsSync, readFileSync, writeFileSync } from '@eventespresso-actions/
 			// plugins/eea-wait-lists/acceptance_tests/Page/WaitListsGeneral.php
 			pathParts = file.filename.split('/');
 
-    		core.info(`filename: ${file.filename}`)
+			core.info(`filename: ${file.filename}`);
 			if (pathParts[0] === 'plugins' && pathParts[1] !== undefined) {
 				pluginName = pathParts[1];
 				if (!plugins.includes(pluginName)) {
 					plugins.push(pluginName);
-					core.info(`plugin: ${pluginName}`)
+					core.info(`plugin: ${pluginName}`);
 				}
 			}
 		}
 
-
 		let changelogPath: string;
 		let changelogEntry: string;
 		let changelogContents: string;
-		const PLACEHOLDER = '<changelog_entry>'
+		const PLACEHOLDER = '<changelog_entry>';
 		for (const plugin of plugins) {
 			changelogContents = PLACEHOLDER;
 			changelogPath = `plugins/${plugin}/CHANGELOG.md`;
-			core.info(`changelogPath: ${changelogPath}`)
+			core.info(`changelogPath: ${changelogPath}`);
 
 			if (existsSync(changelogPath)) {
 				changelogContents = readFileSync(changelogPath, { encoding: 'utf8' });
 			}
 
 			changelogEntry = pullRequest.title;
-			core.info(`changelogEntry: ${changelogEntry}`)
+			core.info(`changelogEntry: ${changelogEntry}`);
 			changelogEntry += PLACEHOLDER;
 
 			changelogContents = changelogContents.replace(PLACEHOLDER, changelogEntry);
 			writeFileSync(changelogPath, changelogContents, { encoding: 'utf8' });
 		}
-
-  } catch (error) {
-	core.setFailed(error.message);
-  }
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 })();
