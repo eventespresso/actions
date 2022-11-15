62a5466f084b1ba813ebf62d3e85a80c5b7daf6a
Brent Christensen <brent@eventespresso.com>
2022-11-14 16:54:23 -0800

    code cleanup

--- packages/setup-ee-addon-tests/setup-ee-addon-tests.sh
+++ packages/setup-ee-addon-tests/setup-ee-addon-tests.sh
@@ -11,17 +11,17 @@ fi
 function eeCoreSetup {
     local BRANCH=$1
     git clone https://github.com/eventespresso/event-espresso-core.git $event_espresso_core_dir
-    cd $event_espresso_core_dir/tests
+    cd $event_espresso_core_dir/tests || exit
     if [ "$BRANCH" = "master" ]; then
         git checkout master
     else
         git fetch --tags
-        git checkout tags/$BRANCH -b $BRANCH
+        git checkout "tags/$BRANCH" -b "$BRANCH"
     fi
     #back to previous directory
-    cd -
-    echo "Building against EE core" $BRANCH
+    cd - || exit
+    echo "Building against EE core" "$BRANCH"
 }
 
 
-eeCoreSetup $EE_VERSION
+eeCoreSetup "$EE_VERSION"
