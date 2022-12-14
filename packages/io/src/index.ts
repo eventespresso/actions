aa5470d14bebb0cebd63a39a1edc8f1f6cb693f8
tn3rb <brent@eventespresso.com>
2022-12-13 19:20:55 -0800

    load writeFile from fs/promises

--- packages/io/src/index.ts
+++ packages/io/src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 export * from '@actions/io';
 
-export const { chmod, copyFile, lstat, mkdir, readdir, readFile, rename, unlink } = fs.promises;
+export const { chmod, copyFile, lstat, mkdir, readdir, readFile, rename, unlink, writeFile } = fs.promises;
 
 export const { readFileSync, readdirSync, existsSync, writeFileSync } = fs;
