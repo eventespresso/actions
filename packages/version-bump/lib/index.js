be7be65051c9b732e4fbc0680492a39f8aa14e09
tn3rb <brent@eventespresso.com>
2022-12-14 15:46:12 -0800

    untrack .gitignore'd files

--- packages/version-bump/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = __importDefault(require("./main"));
-const utils_1 = require("./utils");
-const { mainFile, releaseType: releaseTypeInput, type, value } = (0, utils_1.getInput)();
-(0, main_1.default)(mainFile, releaseTypeInput, type, value);
