4f244f17fb60ecd0f8007704de1948c508a234d0
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-11-21 23:08:31 +0200

    Update Yarn.ts to use GPG encryption

--- packages/e2e-tests/src/ContextFactory.ts
+++ packages/e2e-tests/src/ContextFactory.ts
@@ -5,6 +5,9 @@ import { Git } from './Git';
 import { RepositoryFactory } from './RepositoryFactory';
 import { Yarn } from './Yarn';
 import { Artifact } from './Artifact';
+import { Tar } from './Tar';
+import { GPG } from './GPG';
+import { InputFactory } from './InputFactory';
 
 class ContextFactory {
 	constructor(private readonly repos: RepositoryFactory) {}
@@ -15,7 +18,10 @@ class ContextFactory {
 		const cache = new Cache(repo);
 		const spawn = new SpawnSync(repo.cwd);
 		const artifact = new Artifact();
-		const yarn = new Yarn(repo, spawn, cache, artifact);
+		const tar = new Tar();
+		const inputs = new InputFactory();
+		const gpg = new GPG(inputs);
+		const yarn = new Yarn(repo, spawn, cache, artifact, tar, gpg);
 		return new Context(repo, cache, yarn, git);
 	}
 }
